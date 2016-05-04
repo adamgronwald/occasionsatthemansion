22d497ea80677e9544716de2936b6fed28cd4085
Adam Gronwald <adam.gronwald@gmail.com>
2016-05-04 13:03:11 -0500

    initial commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package HappyBase
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'happybase' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'happybase' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'happybase' ), 'happybase', '<a href="http://happydesigncompany.com" rel="designer">Adam Gronwald</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
